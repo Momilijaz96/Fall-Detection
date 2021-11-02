4906e21a84569be72f7c7cba6068a7d6679cfed9
Momilijaz96 <im.momil@yahoo.com>
2021-11-01 21:58:47 -0400

    Updated Model

--- model.py
+++ model.py
@@ -10,11 +10,11 @@ import torch.nn as nn
 import torch.nn.functional as F
 from model_utils import Block
 
-#Spatial Temporal Transformer Model for Fall Detection
+
 class FallTransformer(nn.Module):
     def __init__(self, num_frame=9, num_joints=17, in_chans=2, embed_dim_ratio=32, depth=4,
                  num_heads=8, mlp_ratio=2., qkv_bias=True, qk_scale=None,
-                 drop_rate=0., attn_drop_rate=0., drop_path_rate=0.2,  norm_layer=None):
+                 drop_rate=0., attn_drop_rate=0., drop_path_rate=0.2,  norm_layer=None, num_classes=2):
         """    ##########hybrid_backbone=None, representation_size=None,
         Args:
             num_frame (int, tuple): input frame number
@@ -33,20 +33,19 @@ class FallTransformer(nn.Module):
         """
         super().__init__()
 
-        norm_layer = norm_layer or partial(nn.LayerNorm, eps=1e-6)
+        norm_layer = norm_layer or partial(nn.LayerNorm, eps=1e-6) #Momil:embed_dim_ratio is spatial transformer's patch embed size
         embed_dim = embed_dim_ratio * num_joints   #### temporal embed_dim is num_joints * spatial embedding dim ratio
-        out_dim = 1 #num_joints * 3     #### output dimension is num_joints * 3
 
         print("Num Frames:",num_frame)
         print("Embed dim ratio",embed_dim_ratio)
         print("Embed dim: ",embed_dim)
 
-
         ### spatial patch embedding
         self.Spatial_patch_to_embedding = nn.Linear(in_chans, embed_dim_ratio)
         self.Spatial_pos_embed = nn.Parameter(torch.zeros(1, num_joints, embed_dim_ratio))
         
-        self.Temporal_pos_embed = nn.Parameter(torch.zeros(1, num_frame, embed_dim))
+        self.Temporal_pos_embed = nn.Parameter(torch.zeros(1, num_frame+1, embed_dim)) #additional pos embedding zero for class token
+        self.class_token_embed = nn.Parameter(torch.zeros(1, 1, embed_dim)) #class token patch embed
         self.pos_drop = nn.Dropout(p=drop_rate)
 
 
@@ -67,20 +66,16 @@ class FallTransformer(nn.Module):
         self.Spatial_norm = norm_layer(embed_dim_ratio)
         self.Temporal_norm = norm_layer(embed_dim)
 
-        ####### A easy way to implement weighted mean
-        self.weighted_mean = torch.nn.Conv1d(in_channels=num_frame, out_channels=1, kernel_size=1)
-
-        #Regression head
-        self.head = nn.Sequential(
+        #Classification head
+        self.class_head = nn.Sequential(
             nn.LayerNorm(embed_dim),
-            nn.Linear(embed_dim , out_dim),
+            nn.Linear(embed_dim, num_classes)
         )
 
 
     def Spatial_forward_features(self, x):
         b, _, f, p = x.shape  ##### b is batch size, f is number of frames, p is number of joints
         x = rearrange(x, 'b c f p  -> (b f) p  c', )
-
         x = self.Spatial_patch_to_embedding(x)
         x += self.Spatial_pos_embed
         x = self.pos_drop(x)
@@ -89,31 +84,31 @@ class FallTransformer(nn.Module):
             x = blk(x)
 
         x = self.Spatial_norm(x)
-        x = rearrange(x, '(b f) w c -> b f (w c)', f=f)
+        x = rearrange(x, '(b f) w c -> b f (w c)', f=f) #BxFx(49xSe)
         return x
 
     def forward_features(self, x):
         b  = x.shape[0]
+        class_token=torch.tile(self.class_token_embed,(b,1,1)) #(B,1,embed_dim)
+        x = torch.cat((x,class_token),dim=1) #(B,F+1,embed_dim)
         x += self.Temporal_pos_embed
         x = self.pos_drop(x)
         for blk in self.blocks:
             x = blk(x)
 
         x = self.Temporal_norm(x)
-        ##### x size [b, f, emb_dim], then take weighted mean on frame dimension, we only predict 3D pose of the center frame
-        x = self.weighted_mean(x)
-        x = x.view(b, 1, -1)
+        
+        ###Extract Class token head from the output
+        x = x[:,-1,:]
+        x = x.view(b, -1) # (Batch_size, class_embedding_size)
         return x
 
-
     def forward(self, x):
-        x = x.permute(0, 3, 1, 2)
+        x = x.permute(0, 3, 1, 2) #Rearranges original tensor according to specified index order
         b, _, _, p = x.shape
         ### now x is [batch_size, 2 channels, receptive frames, joint_num], following image data
         x = self.Spatial_forward_features(x)
         x = self.forward_features(x)
-        x = self.head(x)
+        x = self.class_head(x)
 
-        #x = x.view(b, 1, p, -1)
-
-        return torch.sigmoid(x) 
+        return x 
